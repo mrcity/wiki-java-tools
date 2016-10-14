0e1e769ca5cf9435334f2ca0d31716159bd6906b
marco <falke.marco@gmail.com>
2016-10-14 19:56:16 +0200

    Revert "removing Category:Uploaded with UploadWizard deprecated"
    This reverts commit c5ef1bbc8946fcd05e4d25742b70a99ecccfbc14.
    
    (Placed in the wrong Regex group)

--- src/wiki/Commons.java
+++ src/wiki/Commons.java
@@ -293,11 +293,7 @@ public class Commons {
 	public static final String[][] NEWLINE_CATEGORY_REGEX = { {
 			CASE_INSENSITIVE
 					+ "(\\[\\[category:[^]]+\\]\\]\\n)\\n+(\\[\\[category:)",
-			"$1$2" },
-                        {
-                        CASE_INSENSITIVE
-                                        + "\\[\\[Category:Uploaded with UploadWizard\\]\\]\\n?",
-                        "" } };
+			"$1$2" } };
 	public static final String[][] FORMAT_REGEX = {
 			{ CASE_INSENSITIVE + "</?br( )?(/)?\\\\?>", "<br$1$2>" },
 			{
